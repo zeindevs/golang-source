1c7d309ef3d53cc1924498f200f4bd9836219b5d
zeindevs <zeindevs@gmail.com>
2024-03-25 04:45:32 +0700

    add go-htmx

---
+++ go-htmx/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type Film struct {
+	Title    string
+	Director string
+}
+
+func main() {
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		films := map[string][]Film{
+			"Films": {
+				{Title: "The Godfather", Director: "Francis Ford Coppola"},
+				{Title: "Blade Runner", Director: "Ridley Scott"},
+				{Title: "The Thing", Director: "John Carpenter"},
+			},
+		}
+		tmpl.Execute(w, films)
+	}
+
+	h2 := func(w http.ResponseWriter, r *http.Request) {
+		title := r.PostFormValue("title")
+		director := r.PostFormValue("director")
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
+	}
+
+	http.HandleFunc("/", h1)
+	http.HandleFunc("/add-film/", h2)
+
+	log.Println("Server listening on localhost:8000")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
