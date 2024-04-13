4e2360de0f90f8a83fe2b108808588584adee1e0
zeindevs <zeindevs@gmail.com>
2024-04-13 18:35:41 +0700

    add go-rediz

---
+++ go-rediz/go.mod
@@ -0,0 +1,5 @@
+module github.com/zeindevs/gorediz
+
+go 1.22.2
+
+require github.com/tidwall/resp v0.1.1 // indirect
---
+++ go-rediz/proto.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/tidwall/resp"
+)
+
+const (
+	CommandSET = "SET"
+	CommandGET = "GET"
+)
+
+type Command interface{}
+
+type SetCommand struct {
+	key, val []byte
+}
+
+type GetCommand struct {
+	key []byte
+}
+
+func parseCommand(raw string) (Command, error) {
+	rd := resp.NewReader(bytes.NewBufferString(raw))
+
+	for {
+		v, _, err := rd.ReadValue()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if v.Type() == resp.Array {
+			for _, val := range v.Array() {
+
+				switch val.String() {
+				case CommandGET:
+					if len(v.Array()) != 2 {
+						return nil, fmt.Errorf("invalid number of variables of GET command")
+					}
+					cmd := GetCommand{
+						key: v.Array()[1].Bytes(),
+					}
+					return cmd, nil
+				case CommandSET:
+					if len(v.Array()) != 3 {
+						return nil, fmt.Errorf("invalid number of variables of SET command")
+					}
+					cmd := SetCommand{
+						key: v.Array()[1].Bytes(),
+						val: v.Array()[2].Bytes(),
+					}
+					return cmd, nil
+				}
+			}
+		}
+		break
+	}
+
+	return nil, fmt.Errorf("invalid or unknown command received: %s", raw)
+}
