368c789aed35c770758cad85dc483c1854128151
zeindevs <zeindevs@gmail.com>
2024-04-21 13:54:45 +0700

    update go-rediz

--- go-rediz/server_test.go
+++ go-rediz/server_test.go
@@ -8,8 +8,47 @@ import (
 	"testing"
 	"time"
 
+	"github.com/redis/go-redis/v9"
 	"github.com/zeindevs/gorediz/client"
 )
+
+func TestOfficialRedisClient(t *testing.T) {
+	listenAddr := ":5001"
+	server := NewServer(Config{
+		ListenAddr: listenAddr,
+	})
+	go func() {
+		log.Fatal(server.Start())
+	}()
+	time.Sleep(time.Millisecond * 400)
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("localhost:%s", listenAddr),
+		Password: "",
+		DB:       0,
+	})
+
+	testCases := map[string]string{
+		"foo":  "bar",
+		"a":    "gg",
+		"your": "mom",
+		"step": "dad",
+	}
+
+	for key, val := range testCases {
+		if err := rdb.Set(context.Background(), key, val, 0).Err(); err != nil {
+			t.Fatal(err)
+		}
+
+		newVal, err := rdb.Get(context.Background(), key).Result()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if newVal != val {
+			t.Fatalf("expected %s but got %s", val, newVal)
+		}
+	}
+}
 
 func TestFooBar(t *testing.T) {
 	in := map[string]string{
