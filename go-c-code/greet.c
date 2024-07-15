e434a67ae1739abd0caa42d69bbfd46da6da2b3d
zeindevs <zeindevs@gmail.com>
2024-07-15 21:15:23 +0700

    add: go-c-code

---
+++ go-c-code/greet.c
@@ -0,0 +1,10 @@
+#include "greet.h"
+#include <stdio.h>
+
+int greet(const char *name, int year, char *out) {
+  int n;
+
+  n = sprintf(out, "Greetings, %s from %d! We come in peace :)", name, year);
+
+  return n;
+}
