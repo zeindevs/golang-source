12c54e1de299cf0060c3a969ab20530629e1b371
zeindevs <zeindevs@gmail.com>
2024-07-29 05:46:02 +0700

    update: go-chrome-native-message

--- go-chrome-native-message/popup.js
+++ go-chrome-native-message/popup.js
@@ -2,11 +2,14 @@
   "use strict";
 
   document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("send").addEventListener("click", () => {
-      console.log("send message");
+    document.getElementById("send").addEventListener("click", (e) => {
+      let start = performance.now();
+      e.preventDefault();
       const message = document.getElementById("message").value;
       chrome.runtime.sendMessage({ message: message }, (response) => {
-        alert(JSON.stringify(response));
+        let end = performance.now();
+        document.getElementById("result").innerText =
+          `${JSON.stringify(response)} took: ${end - start}ms`;
         console.log(`Response from native app:`, response);
       });
     });
