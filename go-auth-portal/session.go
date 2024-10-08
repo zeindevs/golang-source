73cb820bb7b8eb89efe888eaf467caf2e485df44
zeindevs <zeindevs@gmail.com>
2024-10-08 09:42:59 +0700

    add: go-auth-portal

---
+++ go-auth-portal/session.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var AuthError = errors.New("Unauthorized")
+
+func Authorize(r *http.Request) error {
+	username := r.FormValue("username")
+	user, ok := users[username]
+	if !ok {
+		return AuthError
+	}
+
+	st, err := r.Cookie("session_token")
+  fmt.Println(st.Value)
+	if err != nil || st.Value == "" || st.Value != user.SessionToken {
+		return AuthError
+	}
+
+	csrf := r.Header.Get("X-CSRF-Token")
+	if csrf != user.CSRFToken || csrf == "" {
+		return AuthError
+	}
+
+	return nil
+}
